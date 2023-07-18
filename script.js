7f3e86134d69ecb469c09c3554cbf42361e76399
Kevansh Gopani <130953386+KevanshGopani@users.noreply.github.com>
2023-07-18 10:18:02 +0530

    created a submission

--- script.js
+++ script.js
@@ -38,7 +38,6 @@ function rot13(encodedStr) {
 	for(let i=0; i<str.lenght:i++){
 		if(lookup[str[i]]==undefined){
 			decodedArr.push(str[i])
- 
 		else decodedArr.push(lookup[str[i]])
 	}
 
@@ -47,7 +46,7 @@ function rot13(encodedStr) {
 
 // You can test your code by running the above function and printing it to console by pressing the run button at the top. To run it with input 36, uncomment the following line
 
-// console.log(rot13("SERR YBIR? NPPVBWBO"));
+console.log(rot13("SERR YBIR? NPPVBWBO"));
 
 // Do not change this line
 window.rot13 = rot13;
